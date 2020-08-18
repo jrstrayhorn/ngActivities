fe45530528a6625798902a2f37df0571b4ed3a71
stepforwardtech <jrstrayhorn@gmail.com>
2020-08-18 15:38:52 -0400

    done - initializing the form with data

--- ngActivitiesSPA/src/app/activities/activity-form/activity-form.component.ts
+++ ngActivitiesSPA/src/app/activities/activity-form/activity-form.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { IActivity } from './../shared/activity.model';
+import {
+  SimpleChanges,
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  Input,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-activity-form',
   templateUrl: './activity-form.component.html',
   styleUrls: ['./activity-form.component.css'],
 })
-export class ActivityFormComponent implements OnInit {
+export class ActivityFormComponent implements OnInit, OnChanges {
+  @Input() currentActivity: IActivity;
   @Output() changedEditMode = new EventEmitter<boolean>();
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // is the first time? Yes but this value might change more than once
+    // this would work the first time but not on the changes to currentActivtiy
+    // need to use OnNgChanges() to check for null and set to empty object
+    // console.log(this.activity);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.currentActivity = this.initializeForm(
+      changes.currentActivity.currentValue
+    );
+  }
 
   setEditMode(isEdit: boolean) {
     this.changedEditMode.emit(isEdit);
+  }
+
+  initializeForm(activity: IActivity): IActivity {
+    if (!activity) {
+      return {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: '',
+      };
+    }
+    return activity;
   }
 }
